1b7bc4d4682d998adca974883acc3ac897da6351
Christian <christiansalazar2005@gmail.com>
2026-07-16 02:48:37 -0600

    Recompensas agregada

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const app = express();
-const port = 3001;
+const port = 3000;
 
 // 1. Cargar Base de Datos Local (JSON)
 const dbPath = path.join(__dirname, 'db.json');
